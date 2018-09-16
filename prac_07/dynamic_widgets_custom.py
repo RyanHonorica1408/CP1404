019d0b65105add22f7ec19a18b5d188e87076c5a
Ryan Honorica <ryan.tiomico@my.jcu.edu.au>
2018-09-16 10:41:31 +1000

    Addition of other prac 7 workspaces

--- prac_07/dynamic_widgets_custom.py
+++ prac_07/dynamic_widgets_custom.py
@@ -7,7 +7,7 @@ from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
 
-class DynamicWidgetsApp(App):
+class DynamicWidgetsApp_custom(App):
     """Main program - Kivy app to demo dynamic widget creation."""
     status_text = StringProperty()
 
@@ -16,9 +16,7 @@ class DynamicWidgetsApp(App):
         Construct main app.
         """
         super().__init__(**kwargs)
-        # basic data example - dictionary of names: phone numbers
-        # TODO: After running it, add another entry to the dictionary and see how the layout changes
-        self.phonebook = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
+        self.name= {"Bob Brown", "Cat Cyan", "Oren Ochre"}
 
     def build(self):
         """
@@ -26,16 +24,16 @@ class DynamicWidgetsApp(App):
         :return: reference to the root Kivy widget
         """
         self.title = "Dynamic Widgets"
-        self.root = Builder.load_file('dynamic_widgets.kv')
+        self.root = Builder.load_file('dynamic_widgets_custom')
         self.create_widgets()
         return self.root
 
     def create_widgets(self):
         """
-        Create buttons from dictionary entries and add them to the GUI
+        Create buttons and add them to the GUI
         :return: None
         """
-        for name in self.phonebook:
+        for name in self.name:
             # create a button for each phonebook entry, specifying the text and id
             # (although text and id are the same in this case, you should see how this works)
             temp_button = Button(text=name, id=name)
@@ -43,23 +41,4 @@ class DynamicWidgetsApp(App):
             # add the button to the "entries_box" using add_widget()
             self.root.ids.entries_box.add_widget(temp_button)
 
-    def press_entry(self, instance):
-        """
-        Handle pressing entry buttons.
-        :param instance: the Kivy button instance
-        :return: None
-        """
-        # get name (dictionary key) from the id of Button we clicked on
-        name = instance.id  # or name = instance.text
-        # update status text
-        self.status_text = "{}'s number is {}".format(name, self.phonebook[name])
-
-    def clear_all(self):
-        """
-        Clear all of the widgets that are children of the "entries_box" layout widget
-        :return:
-        """
-        self.root.ids.entries_box.clear_widgets()
-
-
-DynamicWidgetsApp().run()
+DynamicWidgetsApp_custom().run()
